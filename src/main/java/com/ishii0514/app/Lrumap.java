fe11ae17274f62eb2a661decef26d50333f75209
Yosuke Ishii <ishii0514@gmail.com>
2011-12-03 02:13:04 +0900

    LRUサイズ変更まで

---
+++ src/main/java/com/ishii0514/app/Lrumap.java
@@ -0,0 +1,73 @@
+package com.ishii0514.app;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Lrumap<K,V> {
+
+private int size;
+private Map<K,V> datamap;
+private List<K> history;
+
+public Lrumap(int in_size)
+{
+	if (in_size < 1)
+		throw new IllegalArgumentException();
+
+	size = in_size;
+	datamap = new HashMap<K, V>();
+	history = new LinkedList<K>();
+}
+
+int maxSize()
+{
+	return size;
+}
+
+public void put(K key, V value) {
+	//重複を防ぐため削除しておく
+	remove(key);
+	
+	datamap.put(key, value);
+	history.add(key);
+	checkSize();
+}
+
+public V get(K key) {
+	int num = history.indexOf(key);
+	if (num != -1)
+	{
+		history.remove(num);
+		history.add(key);
+	}
+	return datamap.get(key);
+}
+
+public void ModifySize(int in_size) {
+	if (in_size <= 0)
+		throw new IllegalArgumentException();
+	
+	size=in_size;
+	while(checkSize());
+}
+
+//サイズが大きかったら、１つ削除
+private boolean checkSize()
+{
+	if (history.size() > size)
+	{
+		remove(history.get(0));
+		return true;
+	}
+	return false;
+}
+
+//指定したキーのデータを削除
+private void remove(K key){
+		datamap.remove(key);
+		history.remove(key);
+}
+
+}
---
+++ src/test/java/com/ishii0514/app/LrumapTest.java
@@ -0,0 +1,120 @@
+package com.ishii0514.app;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNull;
+import org.testng.annotations.Test;
+/**
+* Unit test for simple App.
+*/
+public class LrumapTest
+
+{
+
+@Test
+    public void testLRUMap()
+    {
+     Lrumap<String, String> lru = new Lrumap<String,String>(3);
+     assertEquals(3, lru.maxSize());
+     Lrumap<Object,Object> lru1 = new Lrumap<Object, Object>(4);
+     assertEquals(4, lru1.maxSize());
+    }
+
+@Test
+    public void testPutGet()
+    {
+     Lrumap<String, String> lru= new Lrumap<String, String>(2);
+     lru.put("A","testA");
+     assertEquals("testA",lru.get("A"));
+     lru.put("B","testB");
+     assertEquals("testB",lru.get("B"));
+    }
+
+@Test
+    public void testPut3Elements()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(1);
+     lru.put("A", "testA");
+     lru.put("B", "testB");
+     assertNull(lru.get("A"));
+
+     Lrumap<String, String> lru2 = new Lrumap<String, String>(1);
+     lru2.put("B", "testB");
+     lru2.put("A", "testA");
+     assertNull(lru2.get("B"));
+
+     Lrumap<String, String> lru3 = new Lrumap<String, String>(1);
+     lru3.put("A", "testA");
+     lru3.put("B", "testB");
+     lru3.put("C", "testC");
+     assertNull(lru3.get("B"));
+    }
+
+@Test
+    public void testTouch()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(2);
+     lru.put("1","dragon");
+     lru.put("2", "liger");
+     lru.get("1");
+     lru.put("3", "poseidon");
+     assertNull(lru.get("2"));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSize()
+    {
+     new Lrumap<String, String>(0);
+    }
+
+    @Test
+    public void testDuplicate()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(2);
+     lru.put("1","dragon");
+     lru.put("2", "liger");
+     lru.put("1","dog");
+     lru.put("3","giraff");
+
+     assertEquals("dog",lru.get("1"));
+    }
+
+    @Test
+    public void testDynamicSize()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(2);
+     lru.put("A","iii");
+     lru.put("B", "kkk");
+
+     lru.ModifySize(1);
+     assertNull(lru.get("A"));
+
+    }
+    
+    @Test
+    public void testDynamicSize2()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(4);
+     lru.put("A","i");
+     lru.put("B", "j");
+     lru.put("C", "k");
+     lru.put("D", "l");
+
+     lru.ModifySize(1);
+     assertNull(lru.get("A"));
+     assertNull(lru.get("B"));
+     assertNull(lru.get("C"));
+     assertEquals("l",lru.get("D"));
+
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDynamicSize1()
+    {
+     Lrumap<String, String> lru = new Lrumap<String, String>(2);
+     lru.put("A","iii");
+     lru.put("B", "kkk");
+
+     lru.ModifySize(-1);
+    }
+
+}
